c2ae2138167f58775586b198ed386dd1d8f78154
dilyorabonu <dilyorabonu@gmail.com>
2024-07-18 00:02:19 +0500

    change text

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,22 +7,22 @@ function About() {
       <section className="flex-grow py-10 px-4 md:px-8">
         <div className="mb-10">
           <h2 className="text-3xl font-semibold mb-4">Our Story</h2>
-          <p>
+          <test>
             At OnShop, we believe in bringing the joy of cooking to everyone.
             Our platform offers a diverse collection of recipes from different
             cuisines, ensuring that there's something for everyone. Whether
             you're a beginner or an experienced cook, you'll find recipes that
             suit your skill level and taste preferences.
-          </p>
+          </test>
         </div>
         <div>
           <h2 className="text-3xl font-semibold mb-4">Our Mission</h2>
-          <p>
+          <test>
             Our mission is to inspire people to cook at home by providing
             easy-to-follow recipes, step-by-step guides, and tips from
             professional chefs. We aim to make cooking a fun and enjoyable
             experience for all.
-          </p>
+          </test>
         </div>
       </section>
     </div>
